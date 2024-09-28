6a0e823dd04d8b8f0fa971dd6a9e70418f51ead1
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-09-29 00:45:57 +0200

    commit   9er

---
+++ .next/static/chunks/app/[locale]/rechtliches/datenschutz/page-e315234fb7e4235f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[749,409,931,984,243,961,768,131,850,803,479],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
