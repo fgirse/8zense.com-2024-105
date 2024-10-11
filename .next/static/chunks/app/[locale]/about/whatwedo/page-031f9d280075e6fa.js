e29a9c08d1b979e3404bd6505b2c60b2a5d099d7
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-10-11 09:14:23 +0200

    commit 5er

---
+++ .next/static/chunks/app/[locale]/about/whatwedo/page-031f9d280075e6fa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[243,409,931,984,307,262,961,768,131,850,749,479,803],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/[locale]/rechtliches/cookies/page-0b642402114de2af.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[131,409,931,984,307,243,262,961,768,850,749,479,803],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
