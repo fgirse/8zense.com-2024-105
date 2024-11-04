b2c80c3906d24ddf54fd4fec45925a7565af2d4f
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-11-04 23:50:39 +0100

    commit 63b

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("168e68a42238008d")
+/******/ 		__webpack_require__.h = () => ("68da8938e07b7f9a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f1fbeb799edf61a"; }
+/******/ 		__webpack_require__.h = function() { return "8a8f634ba066f1e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
